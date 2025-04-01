d8aaa7f09c6044b8b3d3b06492ef1d89bb56da72
saif amir <saif.amir888@gmail.com>
2025-04-01 07:35:48 +0500

    Create footer.php

---
+++ trash/footer.php
@@ -0,0 +1,13 @@
+    <!-- Footer -->
+    <footer class="footer">
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
+        <div class="social-links">
+            <a href="https://discord.gg/prpk" target="_blank"><i class="fab fa-discord"></i></a>
+            <a href="https://www.youtube.com/@paradiseroleplaypk" target="_blank"><i class="fab fa-youtube"></i></a>
+            <a href="https://www.instagram.com/theparadisehug/" target="_blank"><i class="fab fa-instagram"></i></a>
+        </div>
+    </footer>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
